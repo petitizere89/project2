f5e8081ee2a18ae3fabedfcec8588bcfa2d55819
jay/bull-95 <jaylabull@gmail.com>
2021-08-14 21:54:51 -0400

    Setting things up
    Cart.ts ,and some pathways added, set up signup in login.component.ts

--- angular_grocery/firstthyme/src/app/app-routing.module.ts
+++ angular_grocery/firstthyme/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
-
+  {path:'home', component:HomePageComponent},
   {path:'login', component:LoginPageComponent}
 
 
