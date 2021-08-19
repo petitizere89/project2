d3bfb8101c9619b1074a53f2f08e66f9781b2891
jay/bull-95 <jaylabull@gmail.com>
2021-08-18 20:36:37 -0400

    Style changes and signup json update

--- angular_grocery/firstthyme/src/app/services/user.service.ts
+++ angular_grocery/firstthyme/src/app/services/user.service.ts
@@ -29,7 +29,10 @@ export class UserService {
 
 
   signup(firstName:string, lastName:string, email:string, password: string): Observable<User>{
-    return this.http.post<User>("http://localhost:8080/users/signup", JSON.stringify({firstName, lastName, email, password}))
+    return this.http.post<User>("http://localhost:8080/users/signup", JSON.stringify({firstName, lastName, email, password}),{ 
+        headers: {
+          'Content-Type': 'application/json'
+        }})
     .pipe(catchError((e) => {
       return throwError(e);
     }));
