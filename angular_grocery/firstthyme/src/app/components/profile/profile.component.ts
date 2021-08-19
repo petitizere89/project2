58bfc3dc0603a2fbb4fe697391d2dba8dde76f5b
jay/bull-95 <jaylabull@gmail.com>
2021-08-18 22:21:49 -0400

    added profile component/style changes

---
+++ angular_grocery/firstthyme/src/app/components/profile/profile.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+// Add functions that hide/replace the profile cards on click 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
